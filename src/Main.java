cdeb57e57b655bd9307fbf46f288e070709281aa
nghurtch <nghurtch@broadinstitute.com>
2022-09-06 15:29:07 +0400

    refine Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,16 @@ import java.util.stream.Stream;
 
 public class Main {
 
+    // Often times we create functions, and we'd like to handle the success and failure cases
+    // e.g perform I/O operations such as logging, interacting with databases, servers or other services.
+    // We often end up writing massive boilerplate for null checking / data availability.
+    // Effect data structure can be useful in order to reduce the boilerplate and write more compact code
+    // which is based on composition and laziness.
+
     public static void main(String[] args) {
 
-        // lazy description of computation from Integer to List<Data>
+        // Effect is a lazy data structure which represents the description of computation from Integer to List<Data>.
+        // It may fail with any concrete instance in the Throwable hierarchy.
         Effect<Integer, List<Data>> description =
                 Effect.fromFunction((Integer num) -> num * 5)
                         .compose(Effect.fromFunction(Object::toString))
@@ -20,10 +27,11 @@ public class Main {
                                 .limit(10)
                                 .map(num -> new Data(data.inner + num))
                                 .collect(Collectors.toList())))
-                        .onSuccess(num -> System.out.println("mapped num to Data successfully: " + num))
-                        .onFailure(err -> System.out.println("Failed due to: " + err));
+                        .onSuccess(num -> System.out.println("mapped num to Data successfully: " + num)) // consumer callback if the pipeline will be successful (it's optional)
+                        .onFailure(err -> System.out.println("Failed due to: " + err)); // consumer callback if the pipeline will be unsuccessful (it's optional)
 
-        // evaluation of computation
+        // Calling executeWithArg(T arg) starts executing the pipeline of composed functions.
+        // Since the pipeline may fail it returns Optional<R> for indicating the possible absence of value.
         Optional<List<Data>> maybeData = description.executeWithArg(new java.util.Random().nextInt(100));
 
     }
